7b92c605fd51c0cbf9af11267d55d841318a4f00
okshi <johnstarhafiz@gmail.com>
2023-01-18 19:05:24 +0600

    This repo wrote for reserved key word

---
+++ reservedWords.js
@@ -0,0 +1,5 @@
+let number = 20;
+const name = "Okshi";
+
+/* here let and const are reserve word we can't use it for variable. On-the-other-hands, there are many more reserved keyword present in JS
+ */
