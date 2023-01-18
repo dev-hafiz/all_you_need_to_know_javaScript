5750a321f91614840847933f51eee89d442a8c34
okshi <johnstarhafiz@gmail.com>
2023-01-18 11:14:11 +0600

    This is the variables example that helps to understand the concepts well

---
+++ variables.js
@@ -0,0 +1,12 @@
+let number = 25;
+let string = "Okshi Hafiz known variables well";
+let obj = { name: "Okhsi", age: "23" };
+let array = ["Okshi", "Denniel", "Lawrence", "Cathrine"];
+let func = function () {
+  console.log("Hello World");
+};
+console.log(number);
+console.log(string);
+console.log(obj);
+console.log(array);
+console.log(func);
