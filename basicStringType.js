fb7172af25bb7064e26822551f0d92ade6c94b53
okshi <johnstarhafiz@gmail.com>
2023-01-21 09:54:13 +0600

    This is basic string repository

---
+++ basicStringType.js
@@ -0,0 +1,6 @@
+// String refers to A string is a sequence of characters enclosed in single or double quotes
+
+let str = "String";
+let str2 = "String";
+let str3 = `String`;
+let str4 = String(450);
---
+++ numberType.js
@@ -0,0 +1,17 @@
+/*===========
+    Number 
+============*/
+
+let numberInt = 20;
+let numberFloat = 20.23;
+
+let makeNuber = Number("30");
+
+/*===========
+    NAN 
+============*/
+
+let num = 30;
+let firstName = "Okshi";
+
+console.log(num * firstName); //Output will NAN (Not a Number)
