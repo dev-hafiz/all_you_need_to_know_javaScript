4117abab3c227ebd4fef352416256eaa6bb4b5ac
okshi <johnstarhafiz@gmail.com>
2023-01-21 10:06:19 +0600

    This is Boolean data type code format you can check it out

---
+++ booleanType.js
@@ -0,0 +1,11 @@
+// Assigned the Boolean Value in a variable
+
+let x = true;
+let y = false;
+let z = Boolean(1); //
+
+let a = true;
+let b = false;
+console.log(a && b); //false
+console.log(a || b); // true
+console.log(!a); // false
