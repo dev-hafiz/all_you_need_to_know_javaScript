2a0a13beb72a25c6bc60e02278c4290034fb7d4e
okshi <johnstarhafiz@gmail.com>
2023-02-03 12:44:48 +0600

    This code will helps to understant to learn type conversion concept

---
+++ typeCoversion.js
@@ -0,0 +1,18 @@
+// String to Number and Number to String type conversion
+let strNumber = "123";
+let number = 20;
+
+console.log(strNumber); //string
+console.log(number); //number
+console.log(Number(strNumber)); // convert a string from string to number
+console.log(number.toString()); //convert a number from number to
+console.log(String(number)); //convert a number from number to
+
+//Integer to Floating and Floating to Integeer type conversion
+let intNumber = 20;
+console.log(intNumber);
+console.log(typeof parseFloat(intNumber));
+
+let floatNumber = 29.66;
+console.log(floatNumber);
+console.log(parseInt(floatNumber));
